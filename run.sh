58896d75b201999564d6ed4df3e85b79f76cfdf1
austnbell <austnbell@gmail.com>
2019-10-16 22:05:47 +0200

    changed run

--- run.sh
+++ run.sh
@@ -7,14 +7,14 @@
 
 
 data_dir=`pwd`/data # add data directory 
-train_dir=`pwd`/data/train
+inter_dir=`pwd`/data/inter
 
 . ./cmd.sh
 . ./path.sh
 set -e
 
-mfccdir=`pwd`/mfcc
-vaddir=`pwd`/mfcc
+mfccdir=$data_dir/mfcc
+vaddir=$data_dir/mfcc
 stage=0
 nnet_dir=exp/xvector_nnet_1a/
 num_components=1024 # the number of UBM components (used for VB resegmentation)
@@ -23,26 +23,28 @@ ivector_dim=400 # the dimension of i-vector (used for VB resegmentation)
 # Prepare datasets
 if [ $stage -le 0 ]; then
 	# generate wav files 
-	genWavFile.py $data_dir $train_dir
+	genWavFile.py $data_dir $inter_dir
 
-
-	
-	
-	
 	# generate MFCC features so that we can create the segments file 
 	steps/make_mfcc.sh --mfcc-config conf/mfcc.conf --nj 40 \
 		--cmd "$train_cmd" --write-utt2num-frames true \
-		$train_dir exp/make_mfcc $mfccdir
-   	 utils/fix_data_dir.sh $train_dir
+		$inter_dir exp/make_mfcc $mfccdir
+   	 utils/fix_data_dir.sh $inter_dir
 	
 	# Compute vad
 	sid/compute_vad_decision.sh --nj 40 --cmd "$train_cmd" \
-	       	$train_dir exp/make_vad $vaddir  
-	utils/fix_data_dir.sh $train_dir # maybe only one fix_data_dir for compute_vad_decision and vad_to_segments?
+	       	$inter_dir exp/make_vad $vaddir  
+	utils/fix_data_dir.sh $inter_dir # maybe only one fix_data_dir for compute_vad_decision and vad_to_segments?
+	
+	# prepare features for x-vector training
+	local/nnet3/xvector/prepare_feats.sh --nj 40 --cmd "$train_cmd" \
+      $inter_dir data/cmn exp/cmn
+    cp $inter_dir/vad.scp data/cmn/
+	
 	
 	# create segments file
-	#diarization/vad_to_segments.sh --nj 40 --cmd "$train_cmd" \
-	#	$train_dir $train_dir
-	#utils/fix_data_dir.sh $train_dir
+	diarization/vad_to_segments.sh --nj 40 --cmd "$train_cmd" \
+		$inter_dir $inter_dir
+	utils/fix_data_dir.sh $inter_dir
 	
 fi
