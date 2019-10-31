a1ae87d2bb0c12040ea834963182ac8de90f813a
Austin Bell <austnbell@gmail.com>
2019-10-31 11:36:24 +0000

    created segments

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ set -e
 mfccdir=$data_dir/mfcc
 vaddir=$data_dir/mfcc
 stage=0
-nnet_dir=exp/xvector_nnet_1a/
+nnet_dir=$data_dir/nnet/0003_sre16_v2_1a/exp/xvector_nnet_1a
 num_components=1024 # the number of UBM components (used for VB resegmentation)
 ivector_dim=400 # the dimension of i-vector (used for VB resegmentation)
 
@@ -31,21 +31,43 @@ if [ $stage -le 0 ]; then
 		--cmd "$train_cmd" --write-utt2num-frames true \
 		$inter_dir exp/make_mfcc $mfccdir
    	utils/fix_data_dir.sh $inter_dir
+
+fi
+
+
+# prepare features for X vector extraction
+if [ $stage -le 1 ]; then
 	
 	# Compute vad
 	sid/compute_vad_decision.sh  --cmd "$train_cmd" --nj 8 \
 	       	$inter_dir exp/make_vad $vaddir  
 	utils/fix_data_dir.sh $inter_dir # maybe only one fix_data_dir for compute_vad_decision and vad_to_segments?
 	
+
 	# prepare features for x-vector training
 	local/prepare_feats.sh  --cmd "$train_cmd" --nj 8 \
 	       	$inter_dir data/cmn exp/cmn
-	cp $inter_dir/vad.scp data/cmn/
+	utils/fix_data_dir.sh $inter_dir
+	cp $inter_dir/vad.scp data/cmn	
 	
-	
+
 	# create segments file
 	diarization/vad_to_segments.sh  --cmd "$train_cmd" --nj 8 \
-		$inter_dir $inter_dir
+		$inter_dir  data/cmn/segmented
 	utils/fix_data_dir.sh $inter_dir
+
+	# prepare our cepstral mean normalization for X-vector extraction
+	cp $inter_dir/subsegments data/cmn/segments
+	utils/fix_data_dir.sh data/cmn
 	
 fi
+
+# extract our x-vectors
+if [ $stage -le 2 ]; then
+	diarization/nnet3/xvector/extract_xvectors.sh --cmd "$train_cmd" --nj 8 \
+		--window 1.5 --period 0.75 --apply-cmn false \
+		--min-segment 0.5 $nnet_dir \
+		data/cmn $nnet_dir/exp/xvectors
+fi
+
+
